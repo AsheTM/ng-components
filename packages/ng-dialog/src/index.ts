63c487cfd47d93ac13bb2d6f9acf923bc82bb8d6
AsheTM <haithemalidaoud@gmail.com>
2022-08-09 20:03:42 +0100

    Export everything

--- packages/ng-dialog/src/index.ts
+++ packages/ng-dialog/src/index.ts
@@ -1,7 +1,13 @@
 
 export * from './lib/dialog-common.module';
 export * from './lib/dialog-ref.class';
+export * from './lib/dialog-root.module';
 export * from './lib/dialog.component';
 export * from './lib/dialog.enum';
 export * from './lib/dialog.module';
 export * from './lib/dialog.service';
+
+export * from './lib/dialog-alert';
+export * from './lib/dialog-button';
+export * from './lib/dialog-confirm';
+export * from './lib/dialog-form';
--- packages/ng-dialog/src/lib/dialog-button/index.ts
+++ packages/ng-dialog/src/lib/dialog-button/index.ts
@@ -1,3 +1,4 @@
 
+export * from './dialog-button.component';
 export * from './dialog-button.enum';
 export * from './dialog-button.module';
